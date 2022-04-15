abaaf1b7b5a5229fbfb3d9403e0d2f888b0c8430
Elvis <elvisnipah@gmail.com>
2022-04-15 21:19:51 +0000

    Slight code change

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const container = document.querySelector('#container');
 const resultDiv = document.querySelector(".result");
 const scoreDiv = document.querySelector(".score");
+const paperBtn = document.querySelector('#paperBtn');
+const rockBtn = document.querySelector('#rockBtn');
+const scissorsBtn = document.querySelector('#scissorsBtn');
 
 let playerScore = 0;
 let computerScore = 0;
 function computerPlay() {
     let selection = Math.floor(Math.random() * 3) + 1;
     let compChoice = "";
-        if(selection == 1){
-            return compChoice = "scissors";
-            }
-        else if(selection == 2) {
-            return compChoice = "rock";
-            }
-        else if(selection == 3) {
-            return compChoice = "paper";
-            }
-        else {
-            return undefined;
-            }
+    if(selection == 1){
+        return compChoice = "scissors";
     }
-    
+    else if(selection == 2) {
+        return compChoice = "rock";
+    }
+    else if(selection == 3) {
+        return compChoice = "paper";
+    }
+    else {
+        return undefined;
+    }
+}
+
 let playRound = (playerSelection, computerSelection) => {
     if (playerScore == 5) {
-    resultDiv.textContent = `GAME OVER! You win! Final score is You: ${playerScore} and Computer: ${computerScore}`;
+        resultDiv.textContent = `GAME OVER! You win! Final score is You: ${playerScore} and Computer: ${computerScore}`;
     }
     else if (computerScore == 5) {
         resultDiv.textContent = `GAME OVER! The computer wins! Final score is You: ${playerScore} and Computer: ${computerScore}`;
@@ -49,9 +52,7 @@ let playRound = (playerSelection, computerSelection) => {
     }
     
 }
-const paperBtn = document.querySelector('#paperBtn');
-const rockBtn = document.querySelector('#rockBtn');
-const scissorsBtn = document.querySelector('#scissorsBtn');
+
 paperBtn.addEventListener("click", function() {
     playRound("paper", computerPlay())
 });
